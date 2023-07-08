a31d0045cd751d34727cca4b3019bbbfd79bf23f
Vyzhanov Andrey <ukionik@gmail.com>
2023-07-08 16:05:17 +0300

    Desktop. Добавлен TwitchLib. Добавлены UnitTests

---
+++ ukichat.desktop/UkiChat.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UkiChat.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Console.WriteLine("Hello World!");
+        Assert.Pass();
+    }
+}
