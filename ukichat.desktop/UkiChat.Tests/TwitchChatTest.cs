1f8514ef93b4080842202fb27d3a6ef0e8db424a
Vyzhanov Andrey <ukionik@gmail.com>
2023-07-09 17:31:52 +0300

    Разбираемся с Twitch Api. В процессе...

---
+++ ukichat.desktop/UkiChat.Tests/TwitchChatTest.cs
@@ -0,0 +1,69 @@
+﻿using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+namespace UkiChat.Tests;
+
+public class TwitchChatTest
+{
+    private TwitchClient _client;
+
+    [SetUp]
+    public void Setup()
+    {
+        _client = new TwitchClient();
+        var credentials = new ConnectionCredentials("ukibot", "");
+        var clientOptions = new ClientOptions
+        {
+            MessagesAllowedInPeriod = 750,
+            ThrottlingPeriod = TimeSpan.FromSeconds(30)
+        };
+        var customClient = new WebSocketClient(clientOptions);
+        _client = new TwitchClient(customClient);
+        _client.Initialize(credentials, "F1ashko");
+
+        _client.OnLog += Client_OnLog;
+        _client.OnJoinedChannel += Client_OnJoinedChannel;
+        _client.OnMessageReceived += Client_OnMessageReceived;
+        _client.OnWhisperReceived += Client_OnWhisperReceived;
+        _client.OnNewSubscriber += Client_OnNewSubscriber;
+        _client.OnConnected += Client_OnConnected;
+    }
+
+    private void Client_OnConnected(object? sender, OnConnectedArgs e)
+    {
+        Console.WriteLine("[Connected]");
+    }
+
+    private void Client_OnNewSubscriber(object? sender, OnNewSubscriberArgs e)
+    {
+    }
+
+    private void Client_OnWhisperReceived(object? sender, OnWhisperReceivedArgs e)
+    {
+    }
+
+    private void Client_OnMessageReceived(object? sender, OnMessageReceivedArgs e)
+    {
+        Console.WriteLine($"[Message] {e.ChatMessage}");
+    }
+
+    private void Client_OnJoinedChannel(object? sender, OnJoinedChannelArgs e)
+    {
+        Console.WriteLine($"[Joined Channel] {e.Channel}");
+    }
+
+    private void Client_OnLog(object? sender, OnLogArgs e)
+    {
+        Console.WriteLine($"[Log] {e.Data}");
+    }
+
+    [Test]
+    public async Task ConnectTest()
+    {
+        _client.Connect();
+        await Task.Delay(10 * 60 * 1000);
+    }
+}
